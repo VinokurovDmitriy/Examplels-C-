8719f6aaa2704303490a38f61cfd28ae92d3691d
VinokurovDmitriy <dmitvitvin@gmail.com>
2022-04-21 01:20:14 +0300

    add task 11 Project

---
+++ Example011_sort_array/Program.cs
@@ -0,0 +1,25 @@
+﻿// сортируем массив с цифрами
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+int[] array = { 2, 6, 1, 8, 5, 3, 4, 9, 0, 7, 1, 3 };
+printArray(array);
+for (int i = 0; i < array.Length -1; i++)
+{
+    int minPosition = i;
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[j] < array[minPosition])  minPosition = j;
+    }
+    int temp = array[i];
+    array[i] = array[minPosition];
+    array[minPosition] = temp;
+}
+
+printArray(array);
